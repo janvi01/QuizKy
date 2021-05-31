fa77b2d912040c4d0f45ca71e5386f782b4b3d08
janvi01 <janvibajo1@gmail.com>
2021-05-31 11:48:26 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,42 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
 import Headerr from "./components/header";
 import Quiz from "./components/quiz";
 
-const apii =
-  "https://quizapi.io/api/v1/questions?apiKey=1Mxr4dO4YvLtfodL6scGwHqs5KAMIS4RqT7ZGauq&difficulty=Easy&category=code&limit=20";
 function App() {
-  const [question, setQuestion] = useState([]);
-  const [index, setindex] = useState(0);
-  useEffect(() => {
-    fetch(apii)
-      .then((res) => res.json())
-      .then((data) => {
-        setQuestion(data);
-      });
-  }, []);
-  const [exit, setexit] = useState("");
-
   return (
     <div className="App">
       <Headerr></Headerr>
-      {question.length > 0 ? (
-        <div>
-          <Quiz data={question[index]}></Quiz>
-          <h4>{exit}</h4>
-          <div className="control">
-            <button className="btn" onClick={() => {setindex(index + 1);
-            setexit("");}}>
-              Next question
-            </button>
-            <button className="btn" onClick={() => setexit("Thanks for playing! 🤗")}>
-              Quit
-            </button>
-          </div>
-        </div>
-      ) : (
-        <div className="App-header">Loading.....</div>
-      )}
+      <Quiz ></Quiz>
       <h4>
         Made using{" "}
         <a
--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,75 +1,109 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./quiz.css";
 
-function Quiz({ data: { question, answers, correct_answers } }) {
-  const [score, setscore] = useState(0);
-  const [res, setres] = useState("");
-  const [count, setcount] = useState(0);
-  const [wrong, setwrong] = useState(0);
-  function check(correct, nam) {
-    if (correct === "true") {
-        setcount(count+1); 
-        if(count===0)
-      setscore(score + 1);
-      setres("Yayy! Correct answer 🎉");
-      document.getElementById(nam).className="correct";
-    } else {
-        setwrong(wrong+1);
-        setres("Oops, Wrong answer.");
-        document.getElementById(nam).className="incorrect";
-    }
-  }  
-  function reset(){
-    document.getElementById("a").className="btnn";
-    document.getElementById("b").className="btnn";
-    document.getElementById("c").className="btnn";
-    document.getElementById("d").className="btnn";
-    setres("");
-    setcount(0);
+function Quiz() {
+  const [category, setcategory] = useState("9");
+  const [Heading, setHeading] = useState("General Knowledge");
+  const [questioncontent, setQuestioncontent] = useState([]);
+  const [index, setindex] = useState(0);
+  useEffect(() => {
+    const api_url = `https://opentdb.com/api.php?amount=30&category=${category}`;
+    fetch(api_url)
+      .then((res) => res.json())
+      .then((data) => {
+        setQuestioncontent(data.results);
+      });
+  }, [category]);
+  function generate() {
+    const headinglist = [
+      "General Knowledge",
+      "Sports",
+      "Entertainment",
+      "Science and Nature",
+      "Computers",
+      "Music",
+    ];
+    var index = document.getElementById("cat").selectedIndex;
+    setcategory(document.getElementById("cat").value);
+    setHeading(headinglist[index]);
   }
-  
+  const [show, setshow] = useState(false);
+  const [resultshow, setresultshow] = useState(false);
 
   return (
-    <div className="main">
-      <div className="container">
-        <div className="head">
-          <span>Score : {score}</span><br/>
+    <div className="mid">
+      <h2>Select Category</h2>
+      <select id="cat">
+        <option value="9">General Knowledge</option>
+        <option value="21">Sports</option>
+        <option value="11">Entertainment</option>
+        <option value="17">Science and Nature</option>
+        <option value="18">Computers</option>
+        <option value="12">Music</option>
+      </select>
+      <br />
+      <br />
+      <button
+        className="GenBtn"
+        onClick={() => {
+          generate();
+          setshow(true);
+          setresultshow(false);
+        }}
+      >
+        Generate
+      </button>
+      <div style={{ display: show ? "block" : "none" }}>
+        <h2 className="catTitle">{Heading}</h2>
+        <div
+          className="result"
+          style={{ display: resultshow ? "block" : "none" }}
+        >
+          <h3>Your score is 0/0.</h3>
+          <h3>Thanks for playing! 🤗</h3>
+          <button className="btn2">Play Again</button>
         </div>
-        <div className="topbox">
-          <h2>{question}</h2>
+        <div className="main">
+          {questioncontent.length > 0 ? (
+            <div className="container">
+              <span className="Qno">Q.No. ~ {index + 1}</span>
+              <div className="head">
+                <span>Score : 0</span>
+                <br />
+              </div>
+              <div className="topbox">
+                <h2>{questioncontent[index].question}</h2>
+              </div>
+              <div className="answerbox">
+                <button className="btn">
+                  {questioncontent[index].correct_answer}
+                </button>
+                <button className="btn">
+                  {questioncontent[index].incorrect_answers[0]}
+                </button>
+                <button className="btn">
+                  {questioncontent[index].incorrect_answers[1]}
+                </button>
+                <button className="btn">
+                  {questioncontent[index].incorrect_answers[2]}
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div
+              className="App-header"
+              onClick={() => console.log(questioncontent[index])}
+            >
+              Loading.....
+            </div>
+          )}
         </div>
-        <div className="answerbox">
-          <button className="btnn" id="a"
-            onClick={() => {
-              check(correct_answers.answer_a_correct, "a");
-            }}
-          >
-            {answers.answer_a}
-          </button>
-          <button className="btnn" id="b"
-            onClick={() => {
-              check(correct_answers.answer_b_correct, "b");
-            }}
-          >
-            {answers.answer_b}
-          </button>
-          <button className="btnn" id="c"
-            onClick={() => {
-              check(correct_answers.answer_c_correct, "c");
-            }}
-          >
-            {answers.answer_c}
-          </button>
-          <button className="btnn" id="d"
-            onClick={() => {
-              check(correct_answers.answer_d_correct, "d");
-            }}
-          >
-            {answers.answer_d}
-          </button>
-          <h4>{res}</h4>
-          <i class="fas fa-sync-alt" onClick={reset}></i>
-        </div>
+        <button className="btn2" onClick={() => setindex(index + 1)}>
+          Next
+        </button>
+        <button className="btn2" onClick={() => setresultshow(true)}>
+          Quit
+        </button>
       </div>
     </div>
   );
